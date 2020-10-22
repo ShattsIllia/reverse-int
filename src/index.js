3c70d24168473dd3ce3861ae0f0b0ec06298525d
ShattsIllia <shatts.ilya@gmail.com>
2020-10-22 15:56:14 +0300

    feat: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+//  module.exports = function reverse(n) {
+//          let digit, result = 0;
+
+//          while (n) {
+//              digit = n % 10;
+//              result = (result * 10) + digit;
+//              n = n / 10 | 0;
+//          }
+
+//          return result;
+//      }
+module.exports = function reverse(n) {
+    const stringifietN = n.toString();
+    let result = stringifietN.includes('-') ? stringifietN.slice(1) : stringifietN;
+    return result.split('').reverse().join('');
 }
